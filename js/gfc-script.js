bdb981cd6fabe00ed2c2ff289088bdbf27c675a0
Stephen Ng <digitalstephenn@gmail.com>
2015-05-04 16:03:55 +1000

    Add no results message

--- js/gfc-script.js
+++ js/gfc-script.js
@@ -82,63 +82,68 @@ $(document).ready(function() {
 		$.ajax({
 			type: 'GET',
 			dataType: "json",
-			url: 'https://api.foursquare.com/v2/venues/explore?client_id=KWKGYHXL2IGHDM2KVPEXUOJIABE3G1IW3W1SMB42HSS531BW&client_secret=0WYRHOQ0EQQDUT2V12SPGZ1TIFTRCNVPWVYRO23JXPM1U1J3&v=20130815&ll='+lat+','+long+'&llAcc=1000.0&radius=800&section=coffee&venuePhotos=1&limit=10',
+			url: 'https://api.foursquare.com/v2/venues/explore?client_id=KWKGYHXL2IGHDM2KVPEXUOJIABE3G1IW3W1SMB42HSS531BW&client_secret=0WYRHOQ0EQQDUT2V12SPGZ1TIFTRCNVPWVYRO23JXPM1U1J3&v=20130815&ll='+lat+','+long+'&llAcc=1000.0&radius=70&section=coffee&venuePhotos=1&limit=10',
 			success: function(data2){
 				console.log(data2);
 				
 				var coffeePlaces = data2.response.groups[0].items
 
-				//each method
-				$.each(coffeePlaces, function(index, element){
-					var coffeeName = element.venue.name
-					var coffeeStreet = element.venue.location.formattedAddress[0]
-					var coffeeSurburb = element.venue.location.formattedAddress[1]
-					var coffeeDistance = element.venue.location.distance
-					var coffeeRating = element.venue.rating
-					var coffeeLat = element.venue.location.lat
-					var coffeeLng = element.venue.location.lng
-					var source   = $("#store-template").html();
-					var template = Handlebars.compile(source);
-				    var context = {
-					   storetitle: coffeeName, 
-					   storestreet: coffeeStreet,
-					   storesurburb: coffeeSurburb,
-					   storedistance: coffeeDistance,
-					   storerating: coffeeRating,
-					   coffeelat: coffeeLat,
-					   coffeelng: coffeeLng
-					   };
-				    var html    = template(context);
 				
-				   
-				    $('#location-content-container').append(template(context));
-				})
+				if (coffeePlaces.length > 0){
+						//each method
+						$.each(coffeePlaces, function(index, element){
+							var coffeeName = element.venue.name
+							var coffeeStreet = element.venue.location.formattedAddress[0]
+							var coffeeSurburb = element.venue.location.formattedAddress[1]
+							var coffeeDistance = element.venue.location.distance
+							var coffeeRating = element.venue.rating
+							var coffeeLat = element.venue.location.lat
+							var coffeeLng = element.venue.location.lng
+							var source   = $("#store-template").html();
+							var template = Handlebars.compile(source);
+							var context = {
+							   storetitle: coffeeName, 
+							   storestreet: coffeeStreet,
+							   storesurburb: coffeeSurburb,
+							   storedistance: coffeeDistance,
+							   storerating: coffeeRating,
+							   coffeelat: coffeeLat,
+							   coffeelng: coffeeLng
+							   };
+							var html    = template(context);
+						
+						   
+							$('#location-content-container').append(template(context));
+						})
+						
+						$('.store-results').each(function(i, el) {
+							  $(el).delay( i * 800 ).fadeIn( 1600 );
+							  
+							  var valno = $(this).find('#store-rating-no').text()
+							  var valnoFace = $(this).find('#face')
+							  var valnoText = $(this).find('#store-rating')
+							  console.log(valno)
+							  
+							//Coffee ratings filter
+								if(valno >= 7){
+									$(valnoFace).html('<i class="fa fa-smile-o fa-1"></i>')
+								}else if(valno >= 4 && valno <= 6.9){
+									$(valnoFace).html('<i class="fa fa-meh-o fa-1"></i>')
+								}else if(valno >= 1 && valno <= 3.9){
+									$(valnoFace).html('<i class="fa fa-frown-o fa-1"></i>')
+								}else{
+									$(valnoText).html('n/a<span id="noratings">.</span>')
+								}				
+						}).promise().done(function(){
+											$('.credits').fadeIn()
+										});	
+
+				}else{
+					$('#no-results-container').fadeIn(1000, function(){
+											$('.credits').fadeIn()
+										});	
+				}//end of if/else statement
 				
-				$('.store-results').each(function(i, el) {
-					  $(el).delay( i * 800 ).fadeIn( 1600 );
-					  
-					  var valno = $(this).find('#store-rating-no').text()
-					  var valnoFace = $(this).find('#face')
-					  var valnoText = $(this).find('#store-rating')
-					  console.log(valno)
-					  
-					//Coffee ratings filter
-						if(valno >= 7){
-							$(valnoFace).html('<i class="fa fa-smile-o fa-1"></i>')
-						}else if(valno >= 4 && valno <= 6.9){
-							$(valnoFace).html('<i class="fa fa-meh-o fa-1"></i>')
-						}else if(valno >= 1 && valno <= 3.9){
-							$(valnoFace).html('<i class="fa fa-frown-o fa-1"></i>')
-						}else{
-							$(valnoText).html('n/a<span id="noratings">.</span>')
-						}				
-				}).promise().done(function(){
-									$('.credits').fadeIn()
-									console.log('done');
-								});	
-
-
-
 			}
 		});		
 	}
